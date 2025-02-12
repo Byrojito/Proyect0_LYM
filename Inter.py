6b40507c352d00ee2e8cb75bc91c79f0e998c8e2
Byrojito <f.rojasd@uniandes.edu.co>
2025-02-12 18:25:46 -0500

    cambios variables

--- Inter.py
+++ Inter.py
@@ -1,3 +1,28 @@
+def variables(line_txt,dict_1,inter):
+    if line_txt.count("|") == 2:
+        i = inter
+        delta = False
+        while line_txt[i] < len(line_txt) and delta == False:             
+            dict_1["variables"] =         
+        
+        
+def variables2():
+        
+
+def actions(line_txt):
+    
+    return
+
+def functions(line_txt):
+    
+    return
+
+
+
+
+
+
+
 nombre = "example.txt"
 
 with open('Data/'+nombre, 'r') as archivo:
@@ -6,4 +31,3 @@ with open('Data/'+nombre, 'r') as archivo:
 
         print(linea)
         
-def 
