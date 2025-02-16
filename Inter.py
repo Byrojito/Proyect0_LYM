1933512498d21fff55676076c4c47c32de7f1cd9
Byrojito <f.rojasd@uniandes.edu.co>
2025-02-16 00:21:50 -0500

    dec_var y bloques

--- Inter.py
+++ Inter.py
@@ -1,57 +1,64 @@
+import instrucciones as tools
+
 def start(name_txt):
     dic_var = {
         "variables": []
     }
 
     with open('Data/'+name_txt, 'r') as archivo:
-        for linea in archivo:
-            x = recursive_parcel(linea, dic_var)
+        lineas = archivo.readlines()  # leemos todas las líneas y las almacenamos en una lista
+        index = 0
+        while index < len(lineas):
+            x, index = recursive_parcel(lineas, index, dic_var)
             if x == False:
                 return "Wrong"
         if x == True:
             return "Good"
             
-
-def recursive_parcel(line_txt, dict_1):
-    if len(line_txt) == 0:
-        return True
-    if line_txt[0] == " ":
-        return True
-    if line_txt[0] == "|":
-        return declaracion_variables(line_txt, dict_1)
-    if line_txt[0] == "proc":
-        restante = procesos(line_txt,dict_1)
-        return restante
-    if line_txt[0] == "[":
-        bloques(line_txt, dict_1)
-    else:
-        return False
+def recursive_parcel(lineas, index, dict_1):
+    if index < len(lineas):
+        line_txt = lineas[index].strip()
+        if len(line_txt) != 0:
+            if line_txt[0] == " ":
+                return True, index + 1
+            if line_txt[0] == "|":
+                return declaracion_variables(lineas, index, dict_1)
+            if line_txt[0] == "proc":
+                restante = procesos(line_txt, dict_1)
+                return restante, index + 1
+            if line_txt[0] == "[":
+                return bloques(lineas, dict_1), index + 1
+    return False, index
             
-def declaracion_variables(line_txt, dict_1):
+def declaracion_variables(lineas, index, dict_1):
+    line_txt = lineas[index].strip()
     if line_txt.count("|") == 2:
         variables = line_txt.split('|')[1].strip().split()
         for var in variables:
             dict_1["variables"].append(var)
-        recursive_parcel(line_txt, dict_1)
-            
- 
-        
+        return recursive_parcel(lineas, index + 1, dict_1)  # pasamos a la siguiente línea
+    else:
+        return False, index
+
 def procesos(line_txt, dict_1):
-    
+
+    return True, 0
+
+def bloques(lineas, index, dict_1):
+    line_txt = lineas[index].strip()
+    if len(line_txt) > 2:
+        return False, index
+    else:
+        line = line_txt[index+1].strip()
+        tools.instructions(line, tools.constants)
+        if len(lineas) > index+1:
+            recursive_parcel(lineas, index, dict_1)
+        else:
+            
         
-
-def bloques(line_txt, dict_1):
+        
     
-    return
-
-def functions(line_txt):
-    
-    return
-
-
-
-
-
+    return True, 0
 
 
 
