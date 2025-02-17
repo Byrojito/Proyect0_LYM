70ca2c2c8fc1adb8b1256f37faffcbb55823679b
Byrojito <f.rojasd@uniandes.edu.co>
2025-02-17 11:20:43 -0500

    instrucciones

--- instrucciones.py
+++ instrucciones.py
@@ -1,11 +1,210 @@
+def validacion_de_codigo (line):
+    if "[" in line:
+        bloque(line) == True    # llama a la funcion
+       # pasa de linea 
+    if "proc" in line:
+        procedure(line) == True # llama al funcion 
+        # pasa de linea 
+    if "|" in line:
+        variable_declaration(line) == True
+        # pasa linea
+    pass
+
+
+
 
 constants ={
         "D" : ["#left", "#right", "#around"],
         "X" : ["#balloons", "#chips"],
         "O" : ["#north","#south", "#west", "#east"],
+        "OC": ["#front", "#right", "#left", "#back"], 
+        "DC" : ["#north", "#south", "#west"],
         "variables_locales" : [],
         "variables_globales" : []
     }
+
+
+
+
+
+def verificar_bloques (line):
+    if "[" in line:
+        pass 
+
+def bloque():
+    pass
+
+def variable_declaration(line):
+    pass
+
+def procedure(line):
+    pass
+
+
+
+
+def condition (line):
+    if line.startswith("if:"):
+        return if_condition(line,constants)
+    if line.startswith("while:"):
+        return loop_while(line,constants)
+    if line.startswith("for:"):
+        return loop_for(line)
+    if instructions(line, constants):
+        return True
+    
+
+    else:
+        return False
+
+
+def loop_for(line):
+    line = line.strip()
+    if "repeat:" not in line:
+        return False
+    partes = line.split(" repeat: ", 1)
+    if len(partes) != 2:
+        return False
+    n = partes[0][4:].strip() 
+    bloque = partes[1].strip()
+    if not n.isnumeric():
+        return False
+    if not verificar_bloques(bloque, bloque):
+            return False
+    
+    return True
+
+
+
+
+
+def loop_while(line,constants):
+    line= line.strip()
+    if "do:" not in line:
+        return False
+    partes = line.split(" do: ",1)
+    if len(partes) != 2:
+        return False
+    condicion = partes[0][6:].strip()  
+    bloque1 = partes[1].strip()
+    if not tipos_condiciones(condicion,constants):
+        return False
+    if not verificar_bloques(bloque1, bloque1):
+        return False
+    else:
+        return True
+
+
+
+
+def if_condition(line, constants):
+    line = line.strip()
+    if "then:" not in line or "else:" not in line:
+        return False
+    partes = line.split(" then: ",1)
+    if len(partes) != 2:
+        return False
+    condicion = partes[0][3:].strip()  # Quitar "if:"
+    resto = partes[1].split(" else: ",1)
+    if len(resto) != 2:
+        return False
+    bloque1, bloque2 = resto[0].strip(), resto[1].strip()
+    if not tipos_condiciones(condicion, constants):
+        return False
+    if not verificar_bloques(bloque1, bloque2):
+        return False
+    
+    else:
+        return True
+    
+
+
+def verificar_bloques(bloque1, bloque2):
+    if not instructions(bloque1,constants):
+        return False
+    if not instructions(bloque2,constants):
+        return False
+    return True
+
+
+
+def tipos_condiciones(condicion,constants):
+    condicion = condicion
+
+    
+    if condicion.startswith("facing:"):
+        parte = condicion[7:-1].strip()  # Quitar "facing:"
+        if parte not in constants["O"]:
+            return False
+        return True
+    
+    if condicion.startswith("canPut:"):
+        parte = condicion[7:-1].strip()
+        parte1 = parte.split("ofType:")
+        if len(parte1) != 2:
+            return False
+        x,y = parte1[0].strip(), parte1[1].strip()
+        if not (x.isnumeric() and  y in constants["X"]):
+            return False
+        return True
+
+    if condicion.startswith("canPick:"):
+        parte = condicion[8:-1].strip()
+        parte1 = parte.split("ofType:")
+        if len(parte1) != 2:
+            return False
+        x,y = parte1[0].strip(), parte1[1].strip()
+        if (x.isnumeric() and  y in constants["X"]):
+            return True
+        return False
+
+    if condicion.startswith("canMove:"):
+        if "inDir:" in condicion:
+            parte = condicion[8:-1].strip()
+            parte1 = parte.split("inDir:")
+            if len(parte1) != 2:
+                return False
+            x,y = parte1[0].strip(), parte1[1].strip()
+            if (x.isnumeric() and  y in constants["DC"]):
+                return True
+            return False
+
+        if "toThe:" in condicion:
+            parte = condicion[8:-1].strip()
+            parte1 = parte.split("toThe:")
+            if len(parte1) != 2:
+                return False
+            x,y = parte1[0].strip(), parte1[1].strip()
+            if (x.isnumeric() and  y in constants["OC"]):
+                return True
+            return False
+
+
+    if condicion.startswith("canJump:"):
+        if "inDir:" in condicion:
+            parte = condicion[8:-1].strip()
+            parte1 = parte.split("inDir:")
+            if len(parte1) != 2:
+                return False
+            x,y = parte1[0].strip(), parte1[1].strip()
+            if (x.isnumeric() and  y in constants["DC"]):
+                return True
+            return False
+
+        if "toThe:" in condicion:
+            parte = condicion[8:-1].strip()
+            parte1 = parte.split("toThe:")
+            if len(parte1) != 2:
+                return False
+            x,y = parte1[0].strip(), parte1[1].strip()
+            if not(x.isnumeric() and  y in constants["OC"]):
+                return False
+            return True
+
+    return False
+
+     
+    
 
 def instructions(line:str, constants: dict):
     line = line.strip() 
@@ -25,23 +224,18 @@ def instructions(line:str, constants: dict):
         return pick(line, constants)
     if line.startswith("jump:"):
         return jump(line, constants)
-    if line.startswith("nop:"):
+    if line.startswith("nop"):
         return nop(line)
     else:
         return False
 
 
-def nop (line):
-    if not line.endswith("."):  
-        return False
-    partes = line.split(" ")
-    if len(partes) == 1: 
+def nop(line):
+    line = line.strip()  # Elimina espacios innecesarios
+    if line == "nop .":
         return True
     else:
-        False
-    
-
-
+       return False
 
 
 def jump(line, constants):
@@ -99,8 +293,8 @@ def put(line, constants):
     x,y = partes[0].strip(), partes[1].strip()
     if not (x.isnumeric() and y in constants["X"]):
         return False
-    else: 
-        return True
+    else:
+     return True
         
 
 
@@ -186,12 +380,22 @@ def variable_assignments(line):
 
 
 
+asignaciones = "c := 6 ."
+condicion5 = "canJump: 1 inDir: #west ."
+instruccion= "move: 6 inDir: #south ."
+codigo_if = "if: canJump: 6 inDir: #west . then: move: 1 inDir: #west . else: nop ."
+codigo_while = "while: canMove: 1 inDir: #west . do: jump: 6 inDir: #south ."
+codigo_for ="for: 6 repeat: move: 6 inDir: #south ."
 
+#print(tipos_condiciones(condicion5,constants))
 
-
-
-
+print(condition(asignaciones))
         
+
+
+
+
+
 
 
 
